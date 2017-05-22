167daa7b16fb964cbe83b379b4c66fa2efc763a6
vinothvedhik <vinoth.kumar@mindtree.com>
2017-05-22 09:56:06 +0530

    Module3 handlebarjs assignment

---
+++ app.js
@@ -0,0 +1,20 @@
+var express=require('express');
+var exphbs=require('express-handlebars');
+var routes=require('./routes/route.js');
+
+var app=express();
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+
+app.get('/',routes.home);
+app.get('/:hotel',routes.hotel);
+
+var port = process.env.PORT || 8080;
+
+var server=app.listen(port,function(req,res){
+    console.log('Catch the action at http://localhost:'+port);
+});
+module.exports = app;
